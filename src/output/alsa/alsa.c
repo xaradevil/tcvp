35891fe8ed5d8b2938ac6c40083145b35d8b8e4b
mru <mru>
2003-02-22 00:40:57 +0000

    Added ALSA audio output.

---
+++ src/output/alsa/alsa.c
@@ -0,0 +1,129 @@
+/**
+    Copyright (C) 2003  Michael Ahlberg, Måns Rullgård
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+#define ALSA_PCM_NEW_HW_PARAMS_API 1
+#include <alsa/asoundlib.h>
+
+#include <tcvp.h>
+#include <alsa_tc2.h>
+
+typedef struct alsa_out {
+    snd_pcm_t *pcm;
+    snd_pcm_hw_params_t *hwp;
+    int bpf;
+} alsa_out_t;
+
+static int
+alsa_start(tcvp_pipe_t *p)
+{
+    alsa_out_t *ao = p->private;
+
+    snd_pcm_pause(ao->pcm, 0);
+
+    return 0;
+}
+
+static int
+alsa_stop(tcvp_pipe_t *p)
+{
+    alsa_out_t *ao = p->private;
+
+    snd_pcm_pause(ao->pcm, 1);
+
+    return 0;
+}
+
+static int
+alsa_free(tcvp_pipe_t *p)
+{
+    alsa_out_t *ao = p->private;
+
+    snd_pcm_drop(ao->pcm);
+    snd_pcm_close(ao->pcm);
+    snd_pcm_hw_params_free(ao->hwp);
+    free(ao);
+    free(p);
+
+    return 0;
+}
+
+static int
+alsa_play(tcvp_pipe_t *p, packet_t *pk)
+{
+    alsa_out_t *ao = p->private;
+
+    snd_pcm_writei(ao->pcm, pk->data[0], pk->sizes[0] / ao->bpf);
+    pk->free(pk);
+
+    return 0;
+}
+
+extern tcvp_pipe_t *
+alsa_open(audio_stream_t *as, char *device)
+{
+    tcvp_pipe_t *tp;
+    alsa_out_t *ao;
+    snd_pcm_t *pcm;
+    snd_pcm_hw_params_t *hwp;
+    u_int rate = as->sample_rate;
+    int tmp;
+
+    if(!device)
+	device = "hw:0,0";
+
+    if(snd_pcm_open(&pcm, device, SND_PCM_STREAM_PLAYBACK, 0) != 0)
+	return NULL;
+
+    snd_pcm_hw_params_malloc(&hwp);
+    snd_pcm_hw_params_any(pcm, hwp);
+
+    snd_pcm_hw_params_set_access(pcm, hwp,
+				 SND_PCM_ACCESS_RW_INTERLEAVED);
+    snd_pcm_hw_params_set_format(pcm, hwp, SND_PCM_FORMAT_S16_LE);
+    snd_pcm_hw_params_set_rate_near(pcm, hwp, &rate, &tmp);
+    snd_pcm_hw_params_set_channels(pcm, hwp, as->channels);
+
+    snd_pcm_hw_params_set_period_size(pcm, hwp, 4096, 0);
+    snd_pcm_hw_params_set_periods(pcm, hwp, 4, 0);
+
+    snd_pcm_hw_params(pcm, hwp);
+    snd_pcm_prepare(pcm);
+
+    ao = malloc(sizeof(*ao));
+    ao->pcm = pcm;
+    ao->hwp = hwp;
+    ao->bpf = as->channels * 2;
+
+    tp = malloc(sizeof(*tp));
+    tp->input = alsa_play;
+    tp->start = alsa_start;
+    tp->stop = alsa_stop;
+    tp->free = alsa_free;
+    tp->private = ao;
+
+    return tp;
+}
