aea779233a62c00629a50134a3bed391a49743e5
mru <mru>
2003-03-17 19:08:51 +0000

    Changed frame drop thresholds.

--- src/output/video/video.c
+++ src/output/video/video.c
@@ -60,7 +60,7 @@ static int drops[][DROPLEN] = {
     {1, 1, 1, 0}
 };
 
-static float drop_thresholds[] = {0.5, 0.25, 0.125, 0};
+static float drop_thresholds[] = {0.3, 0.2, 0.1, 0};
 
 static void *
 v_play(void *p)
