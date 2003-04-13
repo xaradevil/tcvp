0452ea20a70f4f79c387015e315a879fecbdc5c3
mru <mru>
2003-04-13 23:53:39 +0000

    Simplify and let X server paint window.

--- src/wm/x11/x11wm.c
+++ src/wm/x11/x11wm.c
@@ -30,8 +30,7 @@
 
 typedef struct x11_wm {
     Display *dpy;
-    Window win;
-    GC bg, ck;
+    Window win, swin;
     int width, height;
     float aspect;
     wm_update_t update;
@@ -49,6 +48,7 @@ x11_event(void *p)
 
     XSelectInput(xwm->dpy, xwm->win, StructureNotifyMask);
     XMapWindow(xwm->dpy, xwm->win);
+    XMapSubwindows(xwm->dpy, xwm->win);
 
     while(run){
 	XEvent xe;
@@ -59,8 +59,6 @@ x11_event(void *p)
 	    XWindowAttributes xwa;
 	    Window foo;
 	    int x, y, w, h;
-	    int b1x, b1y, b1w, b1h;
-	    int b2x, b2y, b2w, b2h;
 	    int ckx, cky;
 	    float wa;
 
@@ -73,39 +71,22 @@ x11_event(void *p)
 		y = 0;
 	    }
 
-	    b1x = b1y = 0;
 	    wa = (float) xwa.width / xwa.height;
 	    if(wa > xwm->aspect){
 		h = xwa.height;
 		w = h * xwm->aspect;
-		b1w = (xwa.width - w) / 2;
-		x += b1w;
-		b2w = xwa.width - w - b1w;
-		b1h = b2h = xwa.height;
-		b2x = b1w + w;
-		b2y = 0;
-		ckx = b1w;
+		ckx = (xwa.width - w) / 2;
+		x += ckx;
 		cky = 0;
 	    } else {
 		w = xwa.width;
 		h = w / xwm->aspect;
-		b1h = (xwa.height - h) / 2;
-		y += b1h;
-		b2h = xwa.height - h - b1h;
-		b1w = b2w = xwa.width;
-		b2x = 0;
-		b2y = b1h + h;
+		cky = (xwa.height - h) / 2;
+		y += cky;
 		ckx = 0;
-		cky = b1h;
 	    }
 
-	    XFillRectangle(xwm->dpy, xwm->win, xwm->bg,
-			   b1x, b1y, b1w, b1h);
-	    XFillRectangle(xwm->dpy, xwm->win, xwm->bg,
-			   b2x, b2y, b2w, b2h);
-	    XFillRectangle(xwm->dpy, xwm->win, xwm->ck,
-			   ckx, cky, w, h);
-
+	    XMoveResizeWindow(xwm->dpy, xwm->swin, ckx, cky, w, h);
 	    xwm->update(xwm->cbd, WM_MOVE, x, y, w, h);
 	    break;
 	}
@@ -165,6 +146,7 @@ x11_open(int width, int height, wm_update_t upd, void *cbd,
     win = XCreateWindow(dpy, RootWindow(dpy, DefaultScreen(dpy)),
 			0, 0, width, height, 0, CopyFromParent,
 			InputOutput, CopyFromParent, 0, NULL);
+    XSetWindowBackground(dpy, win, 0);
 
     xwm = calloc(1, sizeof(*xwm));
     xwm->dpy = dpy;
@@ -177,11 +159,9 @@ x11_open(int width, int height, wm_update_t upd, void *cbd,
     xwm->flags = flags;
     conf_getvalue(cs, "video/color_key", "%i", &xwm->color_key);
 
-    gcv.function = GXcopy;
-    gcv.foreground = 0;
-    xwm->bg = XCreateGC(dpy, win, GCFunction | GCForeground, &gcv);
-    gcv.foreground = xwm->color_key;
-    xwm->ck = XCreateGC(dpy, win, GCFunction | GCForeground, &gcv);
+    xwm->swin = XCreateWindow(dpy, win, 0, 0, width, height, 0, CopyFromParent,
+			      InputOutput, CopyFromParent, 0, NULL);
+    XSetWindowBackground(dpy, xwm->swin, xwm->color_key);
 
     wm = malloc(sizeof(*wm));
     wm->close = x11_close;
