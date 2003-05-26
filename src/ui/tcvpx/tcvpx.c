036e4bc95692e82f4adebd08627f3bda693639d6
mahl <mahl>
2003-05-26 01:44:14 +0000

    Scrolling title/filename

--- src/ui/tcvpx/tcvpx.c
+++ src/ui/tcvpx/tcvpx.c
@@ -29,11 +29,15 @@
 #include <tcvp_event.h>
 #include <tcvpx_tc2.h>
 #include <string.h>
+#include <unistd.h>
 
 #define STOPPED 0
 #define PLAYING 1
 
-static list *widget_list, *bt_list;
+static list *widget_list, *bt_list, *sl_list;
+static int mapped=1;
+
+static int quit = 0;
 
 static player_t *pl;
 static eventq_t qs;
@@ -49,7 +53,7 @@ static int root_height;
 static int depth;
 static int s_time;
 static int p_state = STOPPED;
-static pthread_t xth, eth;
+static pthread_t xth, eth, sth;
 
 static list *files;
 static list_item *flist_curr;
@@ -63,6 +67,10 @@ typedef struct {
 #define TCIMAGEBUTTON 0
 #define TCLABEL       1
 #define TCBACKGROUND  2
+
+#define TCLABELSTANDARD   0
+#define TCLABELSCROLLING  1
+#define TCLABELPINGPONG   2
 
 typedef union _tcwidget_t tcwidget_t;
 typedef int(*onclick_cb_t)(tcwidget_t *, XEvent *);
@@ -115,7 +123,17 @@ typedef struct {
     XftColor xftcolor;
     XftDraw *xftdraw;
     int xoff, yoff;
+    int text_width;
     char *text;
+    int scroll;
+    int scrolling;
+    int s_width;
+    int s_pos;
+    int s_max;
+    int s_space;
+    int s_dir;
+    XftColor scroll_xftcolor;
+    Pixmap s_text;
 } tclabel_t;
 
 typedef struct {
@@ -182,6 +200,8 @@ tcvp_stop(tcwidget_t *p, XEvent *e)
     p_state = STOPPED;
     eventq_send(qs, te);
     tcfree(te);
+    if(p != NULL)
+	change_label(p->common.skin->title, "Stopped");
 
     return 0;
 }
@@ -208,10 +228,9 @@ tcvp_next(tcwidget_t *p, XEvent *e)
 
     if((current_file = list_next(files, &flist_curr))!=NULL) {
 	if(state_tmp == PLAYING) tcvp_play(NULL, NULL);
-	change_label(p->common.skin->title, strrchr(current_file, '/'));
     } else { 
 	p_state = STOPPED;
-	change_label(p->common.skin->title, "");
+	change_label(p->common.skin->title, "Stopped");
     }
 
     return 0;
@@ -226,10 +245,9 @@ tcvp_previous(tcwidget_t *p, XEvent *e)
 
     if((current_file = list_prev(files, &flist_curr))!=NULL) {
 	if(state_tmp == PLAYING) tcvp_play(NULL, NULL);
-	change_label(p->common.skin->title, strrchr(current_file, '/'));
     } else {
 	p_state = STOPPED;
-	change_label(p->common.skin->title, "");
+	change_label(p->common.skin->title, "Stopped");
     }
 
     return 0;
@@ -238,7 +256,7 @@ tcvp_previous(tcwidget_t *p, XEvent *e)
 static int
 tcvp_close(tcwidget_t *p, XEvent *e)
 {
-    tcvp_stop(NULL, NULL);
+    tcvp_stop(p, NULL);
 
     tc2_request(TC2_UNLOAD_ALL, 0);
 
@@ -268,9 +286,7 @@ tcvp_event(void *p)
 		s_time = 0;
 		update_time(skin);
 		if(p_state == PLAYING) {
-		    p_state = STOPPED;
-		    /* FIXME */
-/* 		    tcvp_next(NULL, NULL); */
+		    tcvp_next((tcwidget_t *)skin->playctl[4], NULL);
 		}
 	    }
 	    break;
@@ -280,6 +296,12 @@ tcvp_event(void *p)
 	    update_time(skin);
 	    break;
 
+	case TCVP_LOAD:{
+	    char *title = strrchr(te->load.stream->file, '/')+1;
+	    if(title == NULL) title = te->load.stream->file;
+	    change_label(skin->title, title);
+	    break;
+	}
 	case -1:
 	    r = 0;
 	    break;
@@ -323,10 +345,52 @@ x11_event(void *p)
 	    }
 	    break;
 	}
+
+	case MapNotify:	    
+	    mapped = 1;
+	    break;
+
+	case UnmapNotify:
+	    mapped = 0;
+	    break;
+
 	case DestroyNotify:
 	    run = 0;
 	    break;
 	}
+    }
+
+    return NULL;
+}
+
+
+static void *
+scroll_labels(void *p)
+{
+    while(!quit) {
+	list_item *current=NULL;
+	tcwidget_t *w;
+
+	usleep(100000);
+
+	if(mapped==1){
+	    while((w = list_next(sl_list, &current))!=NULL) {
+		if(w->label.scrolling == TCLABELSCROLLING) {
+		    w->label.s_pos++;
+		    w->label.s_pos %= w->label.s_max;
+		    if(w->common.repaint) w->common.repaint(w);
+		    draw_widget(w);
+		} else if(w->label.scrolling == TCLABELPINGPONG) {
+		    w->label.s_pos += w->label.s_dir;
+		    if(w->label.s_pos % w->label.s_max == 0){
+			w->label.s_dir *= -1;
+		    }
+		    if(w->common.repaint) w->common.repaint(w);
+		    draw_widget(w);
+		}
+	    }
+	}
+	
     }
 
     return NULL;
@@ -371,6 +435,32 @@ alpha_render(unsigned char *src, unsigned char *dest, int width,
 	    dest[pos+1] = (dest[pos+1]*a + src[pos+1]*b)/256;
 	    dest[pos+2] = (dest[pos+2]*a + src[pos+2]*b)/256;
 	    dest[pos+3] = (dest[pos+3]*a + src[pos+3]*b)/256;
+	}
+    }
+    
+    return 0;
+}
+
+
+static int
+alpha_render_text(unsigned char *src, unsigned char *dest, int width,
+		  int height, int depth, uint32_t color)
+{
+    int x,y;
+    unsigned char red, green, blue;
+
+    red =   (color & 0xff);
+    green = (color & 0xff00)>>8;
+    blue =  (color & 0xff0000)>>16;
+
+    for(y=0;y<height;y++){
+	for(x=0;x<width;x++){
+	    int pos = (x+y*width)*4;
+	    int b = src[pos];
+	    int a = 256-b;
+	    dest[pos+0] = (dest[pos+0]*a + red*b)/256;
+	    dest[pos+1] = (dest[pos+1]*a + green*b)/256;
+	    dest[pos+2] = (dest[pos+2]*a + blue*b)/256;
 	}
     }
     
@@ -518,19 +608,19 @@ create_background(skin_t *skin, char *imagefile)
 static int
 repaint_button(tcwidget_t *w)
 {
-    XImage *img;
-
-    img = XGetImage(xd, w->button.skin->background->pixmap,
-		    w->button.x, w->button.y,
-		    w->button.width, w->button.height,
-		    AllPlanes, ZPixmap);
-    alpha_render(*w->button.img->data, img->data, img->width, img->height,
-		 depth);
-    XPutImage(xd, w->button.pixmap, bgc, img, 0, 0, 0, 0,
-	      w->button.img->width, w->button.img->height);
-    XSync(xd, False);
-    XDestroyImage(img);
-
+    if(mapped==1){
+	XImage *img;
+	img = XGetImage(xd, w->button.skin->background->pixmap,
+			w->button.x, w->button.y,
+			w->button.width, w->button.height,
+			AllPlanes, ZPixmap);
+	alpha_render(*w->button.img->data, img->data, img->width,
+		     img->height, depth);
+	XPutImage(xd, w->button.pixmap, bgc, img, 0, 0, 0, 0,
+		  w->button.img->width, w->button.img->height);
+	XSync(xd, False);
+	XDestroyImage(img);
+    }
     return 0;
 }
 
@@ -570,35 +660,162 @@ change_label(tclabel_t *txt, char *text)
 {
     free(txt->text);
     txt->text = strdup((text)?text:"");
-    txt->repaint((tcwidget_t *) txt);
-    draw_widget((tcwidget_t *) txt);
+    if(mapped==1){
+	if(txt->scroll != TCLABELSTANDARD) {
+	    XGlyphInfo xgi;
+	    XftTextExtents8(xd, txt->xftfont, text, strlen(text), &xgi);
+	    txt->text_width = xgi.width+2;
+/* 	    printf("\"%s\"\nwidth:%d height:%d x:%d y:%d xOff:%d yOff:%d\n", */
+/* 		   text, xgi.width, xgi.height, xgi.x, xgi.y, */
+/* 		   xgi.xOff, xgi.yOff); */
 
+	    if(xgi.width+2 > txt->width) {
+		txt->scrolling = txt->scroll;
+		if(txt->scroll == TCLABELSCROLLING) {
+		    txt->s_width = xgi.width + 2;
+		} else if(txt->scroll == TCLABELPINGPONG){
+		    txt->s_width = xgi.width + txt->s_space + 2;
+		}
+		    
+		if(txt->s_text) XFreePixmap(xd, txt->s_text);
+		if(txt->xftdraw) XftDrawDestroy(txt->xftdraw);
+		txt->s_text = XCreatePixmap(xd, xw, txt->s_width,
+					    txt->height, depth);
+		txt->xftdraw = XftDrawCreate(xd, txt->s_text,
+					     DefaultVisual(xd, xs),
+					     DefaultColormap(xd, xs));
+
+		XFillRectangle(xd, txt->s_text, bgc, 0, 0,
+			       txt->s_width, txt->height);
+		if(txt->scroll == TCLABELSCROLLING) {
+		    txt->s_max = txt->s_width + txt->s_space;
+		    txt->s_pos = 0;
+		    XftDrawString8(txt->xftdraw, &txt->scroll_xftcolor,
+				   txt->xftfont, txt->xoff,
+				   txt->yoff, txt->text,
+				   strlen(txt->text));
+		} else if(txt->scroll == TCLABELPINGPONG){
+		    txt->s_max = txt->s_width - txt->width;
+		    txt->s_pos = txt->s_space/2;
+		    txt->s_dir = 1;
+		    XftDrawString8(txt->xftdraw, &txt->scroll_xftcolor,
+				   txt->xftfont, txt->xoff + txt->s_space/2,
+				   txt->yoff, txt->text,
+				   strlen(txt->text));
+		}
+	    } else {
+		txt->xftdraw = XftDrawCreate(xd, txt->pixmap,
+					     DefaultVisual(xd, xs),
+					     DefaultColormap(xd, xs));
+		txt->scrolling = TCLABELSTANDARD;
+	    }		
+	} else {
+	    txt->scrolling = TCLABELSTANDARD;
+	}
+	txt->repaint((tcwidget_t *) txt);
+	draw_widget((tcwidget_t *) txt);
+	XSync(xd, False);
+    }
     return 0;
 }
 
 static int
 repaint_label(tcwidget_t *txt)
 {
-    XCopyArea(xd, txt->label.skin->background->pixmap,
-	      txt->label.pixmap, bgc,
-	      txt->label.x, txt->label.y,
-	      txt->label.width, txt->label.height, 0, 0);
-    XftDrawString8(txt->label.xftdraw, &txt->label.xftcolor,
-		   txt->label.xftfont, txt->label.xoff,
-		   txt->label.yoff, txt->label.text,
-		   strlen(txt->label.text));
-/*     fprintf(stderr, "\"%s\"@(%d,%d) size=(%d,%d) offset=(%d,%d)\n",  */
-/* 	    txt->label.text, txt->label.x, txt->label.y, */
-/* 	    txt->label.width, txt->label.height, */
-/* 	    txt->label.xoff, txt->label.yoff); */
+    if(mapped==1){
+	if(txt->label.scrolling == TCLABELSTANDARD){
+	    XCopyArea(xd, txt->label.skin->background->pixmap,
+		      txt->label.pixmap, bgc,
+		      txt->label.x, txt->label.y,
+		      txt->label.width, txt->label.height, 0, 0);
+	    if(txt->label.scroll == TCLABELSTANDARD){
+		XftDrawString8(txt->label.xftdraw, &txt->label.xftcolor,
+			       txt->label.xftfont, txt->label.xoff,
+			       txt->label.yoff, txt->label.text,
+			       strlen(txt->label.text));
+	    } else {
+		XftDrawString8(txt->label.xftdraw, &txt->label.xftcolor,
+			       txt->label.xftfont, txt->label.xoff + 
+			       (txt->label.width - txt->label.text_width)/2,
+			       txt->label.yoff, txt->label.text,
+			       strlen(txt->label.text));
+	    }
+	} else if(txt->label.scrolling == TCLABELSCROLLING) {
+	    XImage *img, *text;
+	    Pixmap pmap;
 
+	    img = XGetImage(xd, txt->label.skin->background->pixmap,
+			    txt->label.x, txt->label.y,
+			    txt->label.width, txt->label.height,
+			    AllPlanes, ZPixmap);
+
+	    pmap = XCreatePixmap(xd, xw, txt->label.width, txt->label.height,
+				 depth);
+	    XFillRectangle(xd, pmap, bgc, 0, 0,
+			   txt->label.width, txt->label.height);
+	    
+	    if(txt->label.s_pos < txt->label.s_width - txt->label.width) {
+		XCopyArea(xd, txt->label.s_text, pmap, bgc, txt->label.s_pos,
+			  0, txt->label.width, txt->label.height, 0, 0);
+	    } else {
+		if(txt->label.s_pos < txt->label.s_width) {
+		    XCopyArea(xd, txt->label.s_text, pmap, bgc,
+			      txt->label.s_pos, 0,
+			      txt->label.s_width - txt->label.s_pos,
+			      txt->label.height, 0, 0);
+		}
+		if(txt->label.s_pos >= txt->label.s_width + txt->label.s_space - txt->label.width) {
+		    XCopyArea(xd, txt->label.s_text, pmap, bgc, 0, 0,
+			      txt->label.width - (txt->label.s_width - txt->label.s_pos),
+			      txt->label.height,
+			      (txt->label.s_width - txt->label.s_pos + txt->label.s_space), 0);
+		}
+	    }
+
+	    text = XGetImage(xd, pmap, 0, 0,
+			    txt->label.width, txt->label.height,
+			    AllPlanes, ZPixmap);
+	    XSync(xd, False);
+
+	    alpha_render_text(text->data, img->data, txt->label.width,
+			      txt->label.height, depth, txt->label.color);
+
+	    XPutImage(xd, txt->label.pixmap, bgc, img, 0, 0, 0, 0,
+		      txt->label.width, txt->label.height);
+	    XSync(xd, False);
+	    XDestroyImage(img);
+	    XDestroyImage(text);
+	    XFreePixmap(xd, pmap);
+	} else if(txt->label.scrolling == TCLABELPINGPONG) {
+	    XImage *img, *text;
+
+	    img = XGetImage(xd, txt->label.skin->background->pixmap,
+			    txt->label.x, txt->label.y,
+			    txt->label.width, txt->label.height,
+			    AllPlanes, ZPixmap);
+	    text = XGetImage(xd, txt->label.s_text, txt->label.s_pos, 0,
+			    txt->label.width, txt->label.height,
+			    AllPlanes, ZPixmap);
+	    XSync(xd, False);
+
+	    alpha_render_text(text->data, img->data, txt->label.width,
+			      txt->label.height, depth, txt->label.color);
+
+	    XPutImage(xd, txt->label.pixmap, bgc, img, 0, 0, 0, 0,
+		      txt->label.width, txt->label.height);
+	    XSync(xd, False);
+	    XDestroyImage(img);
+	    XDestroyImage(text);
+	}
+    }
     return 0;
 }
 
 static tclabel_t*
 create_label(skin_t *skin, int x, int y, int width, int height,
 	     int xoff, int yoff, char *text, char *font,
-	     double size, uint32_t color, onclick_cb_t onclick)
+	     double size, uint32_t color, int scroll,
+	     onclick_cb_t onclick)
 {
     tclabel_t *txt = calloc(sizeof(tclabel_t), 1);
 
@@ -615,7 +832,11 @@ create_label(skin_t *skin, int x, int y, int width, int height,
     txt->size = size;
     txt->xoff = xoff;
     txt->yoff = yoff;
-    txt->text = strdup((text)?text:"");
+    txt->scroll = scroll;
+    txt->s_pos = 0;
+    txt->s_max = 0;
+    txt->s_space = 20;
+    txt->s_dir = 1;
 
     int red = (txt->color & 0xff);
     int green = (txt->color & 0xff00)>>8;
@@ -628,9 +849,17 @@ create_label(skin_t *skin, int x, int y, int width, int height,
 	.blue = blue + (blue<<8),
 	.alpha = alpha + (alpha<<8)
     };
+    XRenderColor sxrc = {
+	.red = 0xffff,
+	.green = 0xffff,
+	.blue = 0xffff,
+	.alpha = alpha + (alpha<<8)
+    };
 
     XftColorAllocValue(xd, DefaultVisual(xd, xs), DefaultColormap(xd, xs),
 		       &xrc, &txt->xftcolor);
+    XftColorAllocValue(xd, DefaultVisual(xd, xs), DefaultColormap(xd, xs),
+		       &sxrc, &txt->scroll_xftcolor);
 
     txt->xftfont =
 	XftFontOpen(xd, xs, XFT_FAMILY, XftTypeString, txt->font,
@@ -643,14 +872,21 @@ create_label(skin_t *skin, int x, int y, int width, int height,
 
     txt->pixmap = XCreatePixmap(xd, xw, txt->width, txt->height, depth);
 
-    txt->xftdraw = XftDrawCreate(xd, txt->pixmap, DefaultVisual(xd, xs),
-				 DefaultColormap(xd, xs));
+    if(txt->scrolling == TCLABELSTANDARD) {	
+	txt->xftdraw = XftDrawCreate(xd, txt->pixmap, DefaultVisual(xd, xs),
+				     DefaultColormap(xd, xs));
+    }
 
     txt->win = XCreateWindow(xd, xw, txt->x, txt->y,
 			     txt->width, txt->height,
 			     0, CopyFromParent, InputOutput,
 			     CopyFromParent, 0, 0);
 
+    change_label(txt, text);
+
+    if(txt->scroll != TCLABELSTANDARD) {
+	list_push(sl_list, txt);
+    }
     list_push(widget_list, txt);
     if(onclick) list_push(bt_list, txt);
 
@@ -661,9 +897,10 @@ create_label(skin_t *skin, int x, int y, int width, int height,
 static int
 draw_widget(tcwidget_t *w)
 {
-    XCopyArea(xd, w->common.pixmap, w->common.win, bgc, 0, 0,
-	      w->common.width, w->common.height, 0, 0);
-
+    if(mapped==1){
+	XCopyArea(xd, w->common.pixmap, w->common.win, bgc, 0, 0,
+		  w->common.width, w->common.height, 0, 0);
+    }
     return 0;
 }
 
@@ -674,12 +911,15 @@ draw_widgets()
     list_item *current=NULL;
     tcwidget_t *w;
 
-    while((w = list_next(widget_list, &current))!=NULL) {
-	draw_widget(w);
+    if(mapped==1){
+	while((w = list_next(widget_list, &current))!=NULL) {
+	    draw_widget(w);
+	}
     }
 
     return 0;
 }
+
 
 static int
 repaint_widgets()
@@ -687,8 +927,10 @@ repaint_widgets()
     list_item *current=NULL;
     tcwidget_t *w;
 
-    while((w = list_next(widget_list, &current))!=NULL) {
-	if(w->common.repaint) w->common.repaint(w);
+    if(mapped==1){
+	while((w = list_next(widget_list, &current))!=NULL) {
+	    if(w->common.repaint) w->common.repaint(w);
+	}
     }
 
     return 0;
@@ -746,10 +988,11 @@ create_ui(skin_t *skin)
 				     "next.png", tcvp_next);
     skin->playctl[4] = create_button(skin, skin->closepos.x, skin->closepos.y,
 				     "close.png", tcvp_close);
-    skin->time = create_label(skin, 12, 1, 36, 10, 0, 7, "  0:00", "courier",
-			      10.0, 0xFF006030, NULL);
-    skin->title = create_label(skin, 12, 10, 176, 10, 0, 7, NULL, "courier",
-			       10.0, 0xFF006030, NULL);
+    skin->time = create_label(skin, 12, 1, 37, 10, 0, 7, "  0:00", "courier",
+			      10.0, 0xFF006030, TCLABELSTANDARD, NULL);
+    skin->title = create_label(skin, 12, 10, 176, 10, 0, 7, "Stopped",
+			       "courier", 10.0, 0xFF006030,
+			       TCLABELPINGPONG, NULL);
 
     return 0;
 }
@@ -763,6 +1006,7 @@ init_graphics(skin_t *skin)
 
     bt_list = list_new(TC_LOCK_SLOPPY);
     widget_list = list_new(TC_LOCK_SLOPPY);
+    sl_list = list_new(TC_LOCK_SLOPPY);
 
     XInitThreads();
     xd = XOpenDisplay(NULL);
@@ -850,8 +1094,7 @@ tcvpx_init(char *p)
     XMapSubwindows(xd, xw);
 
     update_time(skin);
-    change_label(skin->title, strrchr(current_file, '/'));
-
+    
     repaint_widgets();
     draw_widgets();
 
@@ -860,6 +1103,7 @@ tcvpx_init(char *p)
 
     pthread_create(&xth, NULL, x11_event, skin);
     pthread_create(&eth, NULL, tcvp_event, skin);
+    pthread_create(&sth, NULL, scroll_labels, NULL);
 
     return 0;
 }
@@ -872,6 +1116,9 @@ tcvpx_shdn(void)
     XDestroyWindow(xd, xw);
     XSync(xd, False);
 
+    quit = 1;
+
+    pthread_join(sth, NULL);
     /* FIXME: join event thread */
 /*     pthread_join(eth, NULL); */
     pthread_join(xth, NULL);
