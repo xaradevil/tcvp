c87d248a9eb2358d15decdc1f827cbfea47b90e8
mru <mru>
2003-06-17 16:38:15 +0000

    Maybe fix seeking.

--- src/wm/x11/x11wm.c
+++ src/wm/x11/x11wm.c
@@ -106,10 +106,10 @@ x11_event(void *p)
 		te = tcvp_alloc_event(TCVP_PAUSE);
 		break;
 	    case XK_Up:
-		te = tcvp_alloc_event(TCVP_SEEK, 60000000, TCVP_SEEK_REL);
+		te = tcvp_alloc_event(TCVP_SEEK, 60000000LL, TCVP_SEEK_REL);
 		break;
 	    case XK_Down:
-		te = tcvp_alloc_event(TCVP_SEEK, -60000000, TCVP_SEEK_REL);
+		te = tcvp_alloc_event(TCVP_SEEK, -60000000LL, TCVP_SEEK_REL);
 		break;
 	    case XK_q:
 		te = tcvp_alloc_event(TCVP_CLOSE);
