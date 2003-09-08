d5c93408979a9610cab81849ffef332c9741d8a4
mru <mru>
2003-09-08 12:03:46 +0000

    Watchdog.

---
+++ src/watchdog/wd.c
@@ -0,0 +1,116 @@
+/**
+    Copyright (C) 2003  Michael Ahlberg, Måns Rullgård
+
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+**/
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <tcalloc.h>
+#include <pthread.h>
+#include <sys/time.h>
+#include <sys/types.h>
+#include <tcvp_types.h>
+#include <watchdog_tc2.h>
+
+typedef struct watchdog {
+    pthread_t dog;
+    int bone[2];
+    int timeout;
+} watchdog_t;
+
+static void *
+wd_watch(void *p)
+{
+    watchdog_t *w = p;
+    char buf[4];
+
+    for(;;){
+	struct timeval t = { w->timeout / 1000, (w->timeout % 1000) * 1000 };
+	fd_set fs;
+	FD_ZERO(&fs);
+	FD_SET(w->bone[0], &fs);
+	if(!select(FD_SETSIZE, &fs, NULL, NULL, &t)){
+	    fprintf(stderr, "WATCHDOG: timeout\n");
+	    exit(1);
+	}
+	read(w->bone[0], buf, 1);
+    }
+
+    return NULL;
+}
+
+static int
+wd_input(tcvp_pipe_t *p, packet_t *pk)
+{
+    watchdog_t *w = p->private;
+
+    write(w->bone[1], "f", 1);
+    p->next->input(p->next, pk);
+
+    return 0;
+}
+
+static int
+wd_probe(tcvp_pipe_t *p, packet_t *pk, stream_t *s)
+{
+    p->format = *s;
+    return p->next->probe(p->next, pk, &p->format);
+}
+
+static int
+wd_flush(tcvp_pipe_t *p, int drop)
+{
+    return p->next->flush(p->next, drop);
+}
+
+static void
+wd_free(void *p)
+{
+    tcvp_pipe_t *tp = p;
+    watchdog_t *w = tp->private;
+
+    pthread_cancel(w->dog);
+    pthread_join(w->dog, NULL);
+    close(w->bone[0]);
+    close(w->bone[1]);
+    free(w);
+}
+
+extern tcvp_pipe_t *
+wd_new(stream_t *s, tcconf_section_t *cs, tcvp_timer_t *t,
+       muxed_stream_t *ms)
+{
+    tcvp_pipe_t *p;
+    watchdog_t *w;
+
+    w = calloc(1, sizeof(*w));
+    pipe(w->bone);
+    w->timeout = watchdog_conf_timeout;
+    tcconf_getvalue(cs, "timeout", "%i", &w->timeout);
+    fcntl(w->bone[0], F_SETFL, O_NONBLOCK);
+    pthread_create(&w->dog, NULL, wd_watch, w);
+
+    p = tcallocdz(sizeof(*p), NULL, wd_free);
+    p->format = *s;
+    p->input = wd_input;
+    p->probe = wd_probe;
+    p->flush = wd_flush;
+    p->private = w;
+
+    return p;
+}
