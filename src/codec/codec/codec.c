b76154eae28dbfedeb18ceb37b7e2e63a7fa6308
mru <mru>
2003-10-04 16:12:55 +0000

    Suffix based encoder selection.

--- src/codec/codec/codec.c
+++ src/codec/codec/codec.c
@@ -11,6 +11,9 @@
 #include <tcvp_types.h>
 #include <codec_tc2.h>
 
+#define suffix_map tcvp_codec_conf_suffix
+#define suffix_map_size tcvp_codec_conf_suffix_count
+
 extern tcvp_pipe_t *
 dec_new(stream_t *s, tcconf_section_t *cs, tcvp_timer_t *t, muxed_stream_t *ms)
 {
@@ -29,4 +32,40 @@ dec_new(stream_t *s, tcconf_section_t *cs, tcvp_timer_t *t, muxed_stream_t *ms)
 	return NULL;
 
     return cnew(s, cs, t, ms);
+}
+
+extern tcvp_pipe_t *
+enc_new(stream_t *s, tcconf_section_t *cs, tcvp_timer_t *t, muxed_stream_t *ms)
+{
+    encoder_new_t cnew = NULL;
+    char *name, *sf;
+    char *m = NULL;
+
+    if(tcconf_getvalue(cs, "mux/url", "%s", &name) <= 0)
+	return NULL;
+
+    if((sf = strrchr(name, '.'))){
+	int i;
+	for(i = 0; i < suffix_map_size; i++){
+	    if(!strcmp(sf, suffix_map[i].suffix)){
+		if(s->stream_type == STREAM_TYPE_VIDEO)
+		    m = suffix_map[i].vcodec;
+		else if(s->stream_type == STREAM_TYPE_AUDIO)
+		    m = suffix_map[i].acodec;
+		else
+		    fprintf(stderr, "ENCODE: unknown stream type %i\n",
+			    s->stream_type);
+		break;
+	    }
+	}
+    }
+
+    if(m){
+	char mb[strlen(m) + 8];
+	sprintf(mb, "encoder/%s", m);
+	cnew = tc2_get_symbol(mb, "new");
+    }
+
+    free(name);
+    return cnew? cnew(s, cs, t, ms): NULL;
 }
