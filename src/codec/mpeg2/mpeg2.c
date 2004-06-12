215327e8ae9ee461352869e152e5943b8d5b50d0
mru <mru>
2004-06-12 09:56:36 +0000

    mpeg2: manage frame buffers outside libmpeg2 this is needed in case later stages wish to keep them

--- src/codec/mpeg2/mpeg2.c
+++ src/codec/mpeg2/mpeg2.c
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) 2003  Michael Ahlberg, Måns Rullgård
+    Copyright (C) 2003-2004  Michael Ahlberg, Måns Rullgård
 
     Permission is hereby granted, free of charge, to any person
     obtaining a copy of this software and associated documentation
@@ -31,23 +31,152 @@
 #include <mpeg2dec/mpeg2.h>
 #include <mpeg2_tc2.h>
 
+typedef struct mpeg_buf {
+    uint8_t *data[3];
+    struct mpeg_buf *prev, *next;
+    int refs;
+} mpeg_buf_t;
+
 typedef struct mpeg_dec {
     mpeg2dec_t *mpeg2;
     const mpeg2_info_t *info;
     uint64_t pts;
     int flush;
+    mpeg_buf_t *free_bufs, *used_bufs;
+    int nfree;
+    pthread_mutex_t lock;
 } mpeg_dec_t;
 
 typedef struct mpeg_packet {
     packet_t pk;
-    u_char *data[3];
+    mpeg_buf_t *buf;
     int sizes[3];
+    mpeg_dec_t *mpd;
 } mpeg_packet_t;
+
+static void
+mpeg_release_buf(mpeg_dec_t *mpd, mpeg_buf_t *mb)
+{
+    pthread_mutex_lock(&mpd->lock);
+
+    if(--mb->refs)
+	goto out;
+
+    if(mb->prev)
+	mb->prev->next = mb->next;
+    else
+	mpd->used_bufs = mb->next;
+
+    if(mb->next)
+	mb->next->prev = mb->prev;
+
+    if(mpd->nfree < tcvp_codec_mpeg2_conf_bufpool){
+	mb->prev = NULL;
+	mb->next = mpd->free_bufs;
+	mpd->free_bufs = mb;
+	mpd->nfree++;
+    } else {
+	tc2_print("MPEG2", TC2_PRINT_DEBUG, "%p release\n", mb);
+	tcfree(mb);
+    }
+
+out:
+    pthread_mutex_unlock(&mpd->lock);
+}
+
+static void
+mpeg_free_buf(void *p)
+{
+    mpeg_buf_t *b = p;
+    tc2_print("MPEG2", TC2_PRINT_DEBUG, "%p free\n", b);
+    free(b->data[0]);
+    free(b->data[1]);
+    free(b->data[2]);
+}
+
+static mpeg_buf_t *
+mpeg_alloc(mpeg_dec_t *mpd)
+{
+    const mpeg2_sequence_t *seq = mpd->info->sequence;
+    mpeg_buf_t *mb;
+
+    pthread_mutex_lock(&mpd->lock);
+
+    if(mpd->free_bufs){
+	mb = mpd->free_bufs;
+	mpd->free_bufs = mb->next;
+	mpd->nfree--;
+    } else {
+	mb = tcallocd(sizeof(*mb), NULL, mpeg_free_buf);
+	tc2_print("MPEG2", TC2_PRINT_DEBUG, "%p alloc\n", mb);
+	mb->data[0] = malloc(seq->width * seq->height);
+	mb->data[1] = malloc(seq->chroma_width * seq->chroma_height);
+	mb->data[2] = malloc(seq->chroma_width * seq->chroma_height);
+    }
+
+    mb->refs = 1;
+    mb->next = mpd->used_bufs;
+    mb->prev = NULL;
+    if(mpd->used_bufs)
+	mpd->used_bufs->prev = mb;
+    mpd->used_bufs = mb;
+
+    pthread_mutex_unlock(&mpd->lock);
+
+    return mb;
+}
+
+static void
+mpeg_init_bufs(mpeg_dec_t *mpd)
+{
+    int i;
+
+    mpeg2_custom_fbuf(mpd->mpeg2, 1);
+    for(i = 0; i < 2; i++){
+	mpeg_buf_t *fbuf = mpeg_alloc(mpd);
+	mpeg2_set_buf(mpd->mpeg2, fbuf->data, fbuf);
+    }
+}
+
+static void
+mpeg_free_buflist(mpeg_buf_t *mb)
+{
+    while(mb){
+	mpeg_buf_t *nmb = mb->next;
+	tcfree(mb);
+	mb = nmb;
+    }
+}
+
+static void
+mpeg_flush_bufs(mpeg_dec_t *mpd)
+{
+    pthread_mutex_lock(&mpd->lock);
+
+    tc2_print("MPEG2", TC2_PRINT_DEBUG, "mpeg_flush_bufs free\n");
+    mpeg_free_buflist(mpd->free_bufs);
+    mpd->free_bufs = NULL;
+    mpd->nfree = 0;
+
+    tc2_print("MPEG2", TC2_PRINT_DEBUG, "mpeg_flush_bufs used\n");
+    mpeg_free_buflist(mpd->used_bufs);
+    mpd->used_bufs = NULL;
+
+    pthread_mutex_unlock(&mpd->lock);
+}
+
+static void
+mpeg_free_packet(void *p)
+{
+    mpeg_packet_t *mp = p;
+    mpeg_release_buf(mp->mpd, mp->buf);
+}
 
 extern int
 mpeg_decode(tcvp_pipe_t *p, packet_t *pk)
 {
     mpeg_dec_t *mpd = p->private;
+    mpeg_buf_t *fbuf;
     int state;
 
     if(!pk->data){
@@ -65,7 +194,17 @@ mpeg_decode(tcvp_pipe_t *p, packet_t *pk)
 
     do {
 	state = mpeg2_parse(mpd->mpeg2);
-	if(state == STATE_SLICE || state == STATE_END){
+	switch(state){
+	case STATE_SEQUENCE:
+	    mpeg_init_bufs(mpd);
+	    break;
+	case STATE_PICTURE:
+	    fbuf = mpeg_alloc(mpd);
+	    mpeg2_set_buf(mpd->mpeg2, fbuf->data, fbuf);
+	    break;
+	case STATE_SLICE:
+	case STATE_END:
+#if 0
 	    if(mpd->info->display_picture){
 		if(mpd->flush){
 		    if((mpd->info->display_picture->flags &
@@ -75,17 +214,19 @@ mpeg_decode(tcvp_pipe_t *p, packet_t *pk)
 			mpd->flush = 0;
 		}
 	    }
+#endif
 
 	    if(mpd->info->display_fbuf){
-		mpeg_packet_t *pic = tcallocz(sizeof(*pic));
-		int i;
+		mpeg_packet_t *pic = tcallocdz(sizeof(*pic), NULL,
+					       mpeg_free_packet);
 
+		pic->mpd = mpd;
+		pic->buf = mpd->info->display_fbuf->id;
+		pic->buf->refs++;
 		pic->pk.stream = pk->stream;
-		pic->pk.data = pic->data;
+		pic->pk.data = pic->buf->data;
 		pic->pk.planes = 3;
 		pic->pk.sizes = pic->sizes;
-		for(i = 0; i < 3; i++)
-		    pic->data[i] = mpd->info->display_fbuf->buf[i];
 		pic->sizes[0] = mpd->info->sequence->picture_width;
 		pic->sizes[1] = pic->sizes[0]/2;
 		pic->sizes[2] = pic->sizes[0]/2;
@@ -96,7 +237,7 @@ mpeg_decode(tcvp_pipe_t *p, packet_t *pk)
 		}
 
 		if(mpd->pts != -1LL){
-		    tc2_print("MPEG2", TC2_PRINT_DEBUG, "pts %llu\n",
+		    tc2_print("MPEG2", TC2_PRINT_DEBUG+1, "pts %llu\n",
 			      mpd->pts / 27);
 		    pic->pk.flags = TCVP_PKT_FLAG_PTS;
 		    pic->pk.pts = mpd->pts;
@@ -105,6 +246,11 @@ mpeg_decode(tcvp_pipe_t *p, packet_t *pk)
 		pic->pk.private = pic;
 		p->next->input(p->next, &pic->pk);
 	    }
+
+	    if(mpd->info->discard_fbuf){
+		mpeg_release_buf(mpd, mpd->info->discard_fbuf->id);
+	    }
+	    break;
 	}
     } while(state != STATE_BUFFER);
 
@@ -118,6 +264,7 @@ mpeg_probe(tcvp_pipe_t *p, packet_t *pk, stream_t *s)
     mpeg_dec_t *mpd = p->private;
     int state, ret = PROBE_AGAIN;
     const mpeg2_sequence_t *seq;
+    mpeg_buf_t *fbuf;
 
     mpeg2_buffer(mpd->mpeg2, pk->data[0], pk->data[0] + pk->sizes[0]);
     mpd->info = mpeg2_info(mpd->mpeg2);
@@ -139,7 +286,13 @@ mpeg_probe(tcvp_pipe_t *p, packet_t *pk, stream_t *s)
 	    tcreduce(&p->format.video.frame_rate);
 	    if(!(seq->flags & SEQ_FLAG_PROGRESSIVE_SEQUENCE))
 		p->format.video.flags |= TCVP_STREAM_FLAG_INTERLACED;
+	    mpeg_init_bufs(mpd);
+
 	    ret = PROBE_OK;
+	    break;
+	case STATE_PICTURE:
+	    fbuf = mpeg_alloc(mpd);
+	    mpeg2_set_buf(mpd->mpeg2, fbuf->data, fbuf);
 	    break;
 	case STATE_INVALID:
 	    ret = PROBE_AGAIN;
@@ -156,8 +309,12 @@ mpeg_flush(tcvp_pipe_t *p, int drop)
     mpeg_dec_t *mpd = p->private;
 
     if(drop){
-	mpd->flush = 1;
+	mpeg2_reset(mpd->mpeg2, 1);
+/* 	mpd->flush = 1; */
 	mpd->pts = 0;
+	/* FIXME: the buffers should be flushed here, but the reference
+	   counting becomes a nightmare */
+/* 	mpeg_flush_bufs(mpd); */
     }
 
     return 0;
@@ -169,6 +326,7 @@ mpeg_free(void *p)
     mpeg_dec_t *mpd = p;
 
     mpeg2_close(mpd->mpeg2);
+    mpeg_flush_bufs(mpd);
 }
 
 extern int
@@ -180,6 +338,7 @@ mpeg_new(tcvp_pipe_t *p, stream_t *s, tcconf_section_t *cs, tcvp_timer_t *t,
     mpd = tcallocdz(sizeof(*mpd), NULL, mpeg_free);
     mpd->mpeg2 = mpeg2_init();
     mpd->pts = -1LL;
+    pthread_mutex_init(&mpd->lock, NULL);
 
     p->format.common.codec = "video/raw-i420";
     p->private = mpd;
