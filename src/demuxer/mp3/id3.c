98f0bb1af2339973d87c08359ebf9e382bfcd5cd
mru <mru>
2005-03-09 00:52:10 +0000

    id3: read/write more tags, cleanup

--- src/demuxer/mp3/id3.c
+++ src/demuxer/mp3/id3.c
@@ -1,5 +1,5 @@
 /**
-    Copyright (C) 2003, 2004  Michael Ahlberg, Måns Rullgård
+    Copyright (C) 2003-2005  Michael Ahlberg, Måns Rullgård
 
     Permission is hereby granted, free of charge, to any person
     obtaining a copy of this software and associated documentation
@@ -34,27 +34,11 @@
 #include <tcvp_types.h>
 #include <mp3_tc2.h>
 
-#define get4c(t,f) f->read(&t, 4, 1, f)
-
-#define getuint(s)				\
-static inline uint##s##_t			\
-getu##s(url_t *f)				\
-{						\
-    uint##s##_t v;				\
-    f->read(&v, sizeof(v), 1, f);		\
-    v = htob_##s(v);				\
-    return v;					\
-}
-
-getuint(16)
-getuint(32)
-getuint(64)
-
 static inline uint32_t
 getss32(url_t *f, int version)
 {
     uint32_t v;
-    v = getu32(f);
+    url_getu32b(f, &v);
     if(version > 3){
 	v = (v & 0x7fffff) | ((v & ~0xffffff) >> 1);
 	v = (v & 0x7fff) | ((v & ~0xffff) >> 1);
@@ -73,19 +57,6 @@ getss32(url_t *f, int version)
 #define ID3v2_FFLAG_CRYPT (1<<2)
 #define ID3v2_FFLAG_USYNC (1<<1)
 #define ID3v2_FFLAG_DLEN  (1<<0)
-
-#define TAG(a,b,c,d) (d + (c << 8) + (b << 16) + (a << 24))
-
-static inline char *
-tag2str(char *s, uint32_t tag)
-{
-    s[0] = (tag >> 24) & 0xff;
-    s[1] = (tag >> 16) & 0xff;
-    s[2] = (tag >> 8) & 0xff;
-    s[3] = tag & 0xff;
-    s[4] = 0;
-    return s;
-}
 
 static char *
 id3v2_getframe(url_t *f, int *fsize, int fflags)
@@ -189,12 +160,22 @@ id3v2_gettext(u_char *buf, int size)
     return conv_str(buf + 1, size - 1, enc, "UTF-8");
 }
 
+static char *id3v2_tags[][2] = {
+    { "TPE1", "artist" },
+    { "TIT2", "title" },
+    { "TALB", "album" },
+    { "TCON", "genre" },
+    { "TYER", "year" },
+    { "TRCK", "track" },
+    {}
+};
+
 extern int
 id3v2_tag(url_t *f, muxed_stream_t *ms)
 {
     off_t spos;
     char tag[4];
-    int version;
+    uint16_t version;
     int flags;
     int32_t size, tsize;
 
@@ -206,7 +187,7 @@ id3v2_tag(url_t *f, muxed_stream_t *ms)
     if(strncmp(tag, "ID3", 3))
 	goto err;		/* FIXME: check for trailing tag */
 
-    version = getu16(f);
+    url_getu16b(f, &version);
     if(version >= 0x0500 || version < 0x0300){
 	tc2_print("ID3", TC2_PRINT_ERROR,
 		  "Unsupported ID3v2 tag version %i.%i.\n",
@@ -233,23 +214,25 @@ id3v2_tag(url_t *f, muxed_stream_t *ms)
 	size -= esize;
     }
 
-    while(size > 0){
-	uint32_t tag = getu32(f);
+    while(size > 10){
 	uint32_t fsize, dsize;
-	int fflags, dlen = 0;
-	u_char *data = NULL;
+	uint16_t fflags;
+	int dlen = 0;
 	off_t pos;
-	char stag[5];
+	char tag[5];
+	int i;
 
-	if(!tag)
+	if(f->read(tag, 1, 4, f) < 4)
 	    break;
 
+	tag[4] = 0;
+
 	dsize = fsize = getss32(f, version);
-	fflags = getu16(f);
+	url_getu16b(f, &fflags);
 	pos = f->tell(f);
 
 	tc2_print("ID3", TC2_PRINT_DEBUG, "tag %s size=%x flags=%x\n",
-		  tag2str(stag, tag), fsize, fflags);
+		  tag, fsize, fflags);
 
 	if(fflags & ID3v2_FFLAG_GID)
 	    url_getc(f);
@@ -258,42 +241,15 @@ id3v2_tag(url_t *f, muxed_stream_t *ms)
 	if(fflags & ID3v2_FFLAG_DLEN)
 	    dlen = getss32(f, version);
 
-	switch(tag){
-	case TAG('T','I','T','2'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "title", id3v2_gettext(data, dsize), NULL, free);
+	for(i = 0; id3v2_tags[i][0]; i++)
+	    if(!memcmp(tag, id3v2_tags[i][0], 4))
+		break;
+
+	if(id3v2_tags[i][0]){
+	    u_char *data = id3v2_getframe(f, &dsize, fflags);
+	    char *text = id3v2_gettext(data, dsize);
+	    tcattr_set(ms, id3v2_tags[i][1], text, NULL, free);
 	    free(data);
-	    break;
-	case TAG('T','P','E','1'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "performer", id3v2_gettext(data, dsize),
-		       NULL, free);
-	    free(data);
-	    break;
-	case TAG('T','A','L','B'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "album", id3v2_gettext(data, dsize),
-		       NULL, free);
-	    free(data);
-	    break;
-	case TAG('T','C','O','N'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "genre", id3v2_gettext(data, dsize),
-		       NULL, free);
-	    free(data);
-	    break;
-	case TAG('T','Y','E','R'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "year", id3v2_gettext(data, dsize),
-		       NULL, free);
-	    free(data);
-	    break;
-	case TAG('T','R','C','K'):
-	    data = id3v2_getframe(f, &dsize, fflags);
-	    tcattr_set(ms, "track", id3v2_gettext(data, dsize),
-		       NULL, free);
-	    free(data);
-	    break;
 	}
 
 	f->seek(f, pos + fsize, SEEK_SET);
@@ -337,28 +293,17 @@ id3v2_text_frame(char *tag, char *id, char *text)
 extern int
 id3v2_write_tag(url_t *u, muxed_stream_t *ms)
 {
-    char *artist, *title, *album;
     int tagsize;
     char *tag, *p;
-
-    artist = tcattr_get(ms, "artist");
-    title = tcattr_get(ms, "title");
-    album = tcattr_get(ms, "album");
-
-    if(artist && !strlen(artist))
-	artist = NULL;
-    if(title && !strlen(title))
-	title = NULL;
-    if(album && !strlen(album))
-	album = NULL;
+    int i;
 
     tagsize = 10;
-    if(artist)
-	tagsize += 11 + strlen(artist);
-    if(title)
-	tagsize += 11 + strlen(title);
-    if(album)
-	tagsize += 11 + strlen(album);
+
+    for(i = 0; id3v2_tags[i][0]; i++){
+	char *av = tcattr_get(ms, id3v2_tags[i][1]);
+	if(av)
+	    tagsize += 11 + strlen(av);
+    }
 
     if(tagsize <= 10)
 	return 0;
@@ -370,14 +315,15 @@ id3v2_write_tag(url_t *u, muxed_stream_t *ms)
     st_ss32(tagsize - 10, p);
     p += 4;
 
-    if(artist)
-	p += id3v2_text_frame(p, "TPE1", artist);
-    if(title)
-	p += id3v2_text_frame(p, "TIT2", title);
-    if(album)
-	p += id3v2_text_frame(p, "TALB", album);
+    for(i = 0; id3v2_tags[i][0]; i++){
+	char *av = tcattr_get(ms, id3v2_tags[i][1]);
+	if(av)
+	    p += id3v2_text_frame(p, id3v2_tags[i][0], av);
+    }
 
     u->write(tag, 1, tagsize, u);
+
+    free(tag);
     return 0;
 }
 
@@ -389,27 +335,52 @@ id3v1_strdup(char *p, int s)
     while(!(*e & ~0x20) && e > p)
 	e--;
     s = e - p + 1;
+    if(e == p && !(*e & ~0x20))
+	return NULL;
     return conv_str(p, s, tcvp_demux_mp3_conf_id3v1_encoding, "UTF-8");
+}
+
+static int
+id3v1_setattr(void *p, char *attr, char *s, int l)
+{
+    char *v;
+
+    if(tcattr_get(p, attr))
+	return 0;
+
+    v = id3v1_strdup(s, l);
+    if(!v)
+	return 0;
+
+    tcattr_set(p, attr, v, NULL, free);
+
+    return 0;
 }
 
 extern int
 id3v1_tag(url_t *f, muxed_stream_t *ms)
 {
     uint64_t pos = f->tell(f);
-    char buf[128];
+    u_char buf[128];
     int ts = 0;
 
     f->seek(f, -128, SEEK_END);
     f->read(buf, 1, 128, f);
     if(!strncmp(buf, "TAG", 3)){
-	if(!tcattr_get(ms, "title"))
-	    tcattr_set(ms, "title", id3v1_strdup(buf + 3, 30), NULL, free);
-	if(!tcattr_get(ms, "performer"))
-	    tcattr_set(ms, "performer", id3v1_strdup(buf + 33, 30),
-		       NULL, free);
-	if(!tcattr_get(ms, "album"))
-	    tcattr_set(ms, "album", id3v1_strdup(buf + 63, 30),
-		       NULL, free);
+	int trk = buf[126];
+
+	id3v1_setattr(ms, "title", buf + 3, 30);
+	id3v1_setattr(ms, "artist", buf + 33, 30);
+	id3v1_setattr(ms, "album", buf + 63, 30);
+	id3v1_setattr(ms, "year", buf + 93, 4);
+	id3v1_setattr(ms, "comment", buf + 97, 29);
+
+	if(trk > 0 && trk < 100 && !tcattr_get(ms, "track")){
+	    char *track = malloc(4);
+	    snprintf(track, 4, "%i", trk);
+	    tcattr_set(ms, "track", track, NULL, free);
+	}
+
 	ts = 128;
     }
 
